3920f262147fa71e1d9bb858f4255f5b1045a229
sharmaprashant18 <ps3348644@gmail.com>
2024-12-30 00:29:36 +0545

    Changed

--- lib/services/dio.dart
+++ lib/services/dio.dart
@@ -2,10 +2,9 @@ import 'package:dio/dio.dart';
 
 class WeatherService {
   static Dio dio = Dio();
-  static Future getWeather() async {
+  static Future getWeather({required apiPath}) async {
     try {
-      final response = await dio
-          .get('https://api.weatherapi.com/v1/current.json', queryParameters: {
+      final response = await dio.get(apiPath, queryParameters: {
         'api_key': 'ffa8bc7cc16b486d85f103719242912',
       });
     } catch (e) {
