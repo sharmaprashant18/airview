e9e44c21f75624801cb4d57718ca7058a8a6dd14
sharmaprashant18 <ps3348644@gmail.com>
2025-01-03 09:25:50 +0545

    Changed

--- lib/services/dio.dart
+++ lib/services/dio.dart
@@ -9,8 +9,10 @@ class WeatherService {
     try {
       final response = await dio.get(apiPath,
           queryParameters: {'key': 'ffa8bc7cc16b486d85f103719242912', 'q': q});
-      final newData = (response.data['location']);
-      print(newData[0]);
+      final newData = (response.data['location'] as List)
+          .map((e) => WeatherModel.fromJson(e))
+          .toList();
+
       return Right(newData);
     } on DioException catch (e) {
       return Left(e.message as String);
